79926fedb68c928758b1af29a53ad5ec5eb37e4c
JacobBITLABS <36268638+JacobBITLABS@users.noreply.github.com>
2024-12-30 14:09:40 +0000

    Deploying to gh-pages from @ JacobBITLABS/JacobBITLABS.github.io@8355f74927176b546b39665db9505816c44889ca 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -492,7 +492,14 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%6A%61%63%6F%62@%62%69%74%6C%61%62%73.%64%6B", "_blank");
+        },
+      },{
+        id: 'social-github',
+        title: 'GitHub',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://github.com/JacobBITLABS", "_blank");
         },
       },{
         id: 'social-inspire',
@@ -500,6 +507,20 @@ ninja.data = [{
         section: 'Socials',
         handler: () => {
           window.open("https://inspirehep.net/authors/1010907", "_blank");
+        },
+      },{
+        id: 'social-linkedin',
+        title: 'LinkedIn',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://www.linkedin.com/in/jacob-nielsen-681478114", "_blank");
+        },
+      },{
+        id: 'social-orcid',
+        title: 'ORCID',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://orcid.org/0009-0009-8141-630X", "_blank");
         },
       },{
         id: 'social-rss',
@@ -514,13 +535,6 @@ ninja.data = [{
         section: 'Socials',
         handler: () => {
           window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
         },
       },{
       id: 'light-theme',
